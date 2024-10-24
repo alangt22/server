527be645dfd8c66b580d4f0ab8f0985d0f5e11b6
alan nunes <alansilva2896@gmail.com>
2024-10-24 20:32:18 -0300

    3

--- api/parties/index.js
+++ api/parties/index.js
@@ -1,13 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import cors from 'cors';
-import partyController from '../../controllers/partyController'; // ajuste o caminho
+import partyController from '../../controllers/partyController'; // ajuste o caminho conforme necessário
 import conn from '../../db/conn'; // ajuste o caminho conforme necessário
 
 // Habilitar CORS
-const corsMiddleware = cors();
+const corsMiddleware = cors({
+  origin: '*', // Permite todas as origens (ajuste conforme necessário)
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type'], // Cabeçalhos permitidos
+});
+
 const runMiddleware = (req, res, fn) =>
   new Promise((resolve, reject) => {
-    fn(req, res, result => {
+    fn(req, res, (result) => {
       if (result instanceof Error) {
         return reject(result);
       }
@@ -34,6 +39,9 @@ export default async (req, res) => {
       return partyController.delete(req, res);
     case 'PUT':
       return partyController.update(req, res);
+    case 'OPTIONS': // Resposta para requisições preflight
+      res.status(200).end();
+      break;
     default:
       res.setHeader('Allow', ['POST', 'GET', 'DELETE', 'PUT']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
