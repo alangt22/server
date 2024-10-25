bf4a58e3de2489d59fb5b1ff87aa829667b2feac
alan nunes <alansilva2896@gmail.com>
2024-10-24 21:34:49 -0300

    13

--- api/parties/id.js
+++ api/parties/id.js
@@ -3,7 +3,8 @@ import conn from '../../../db/conn';
 import cors from 'cors';
 
 const corsMiddleware = cors();
-const runMiddleware = (req, res, fn) =>
+
+const runMiddleware = (req, res, fn) => 
   new Promise((resolve, reject) => {
     fn(req, res, result => {
       if (result instanceof Error) {
@@ -14,15 +15,24 @@ const runMiddleware = (req, res, fn) =>
   });
 
 export default async (req, res) => {
+  // Conectar ao banco de dados
   await conn();
+
+  // Habilitar CORS
   await runMiddleware(req, res, corsMiddleware);
 
+  // Roteamento baseado no método da requisição
   switch (req.method) {
     case 'GET':
       return partyController.get(req, res);
-    // Outros métodos (POST, DELETE, PUT) se necessário
+    case 'POST':
+      return partyController.create(req, res);
+    case 'DELETE':
+      return partyController.delete(req, res);
+    case 'PUT':
+      return partyController.update(req, res);
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['GET', 'POST', 'DELETE', 'PUT']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
